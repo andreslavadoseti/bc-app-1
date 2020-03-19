cdd06677f0b14ba5ef0a4ef2496ee72b567ccbcd
andreslavado <andreslavado@seti.com.co>
2020-03-18 23:37:33 -0500

    Add hostname and ip variables

--- src/main/java/com/co/seti/sandbox/web/app1/InfoBean.java
+++ src/main/java/com/co/seti/sandbox/web/app1/InfoBean.java
@@ -6,9 +6,13 @@
 package com.co.seti.sandbox.web.app1;
 
 import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -19,12 +23,21 @@ import javax.inject.Named;
  */
 @Named(value = "infoBean")
 @SessionScoped
-public class InfoBean implements Serializable{
+public class InfoBean implements Serializable {
+
     private static final long serialVersionUID = 57867789989L;
     List<EnvironmentVariable> variables = new ArrayList<>();
-    
+
     @PostConstruct
-    private void init(){
+    private void init() {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            String hostname = ip.getHostName();
+            variables.add(new EnvironmentVariable("IP_ADDRESS", ip.toString()));
+            variables.add(new EnvironmentVariable("HOSTNAME", hostname));
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(InfoBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
         Map<String, String> environment = System.getenv();
         for (Map.Entry<String, String> entry : environment.entrySet()) {
             variables.add(new EnvironmentVariable(entry.getKey(), entry.getValue()));
@@ -34,6 +47,5 @@ public class InfoBean implements Serializable{
     public List<EnvironmentVariable> getVariables() {
         return variables;
     }
-    
-    
+
 }
